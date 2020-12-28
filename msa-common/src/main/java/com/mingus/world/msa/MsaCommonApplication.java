874982d57738f53794436b96d7dc94065bba1614
naruduru <naruduru@gmail.com>
2020-12-28 17:12:37 +0900

    Initial commit

---
+++ msa-common/src/main/java/com/mingus/world/msa/MsaCommonApplication.java
@@ -0,0 +1,11 @@
+package com.mingus.world.msa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsaCommonApplication {
+    public static void main(String[] arg) {
+        SpringApplication.run(MsaCommonApplication.class, arg);
+    }
+}
---
+++ msa-ladder/src/main/java/com/mingus/world/msa/MsaLadderApplication.java
@@ -0,0 +1,11 @@
+package com.mingus.world.msa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsaLadderApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(MsaLadderApplication.class, args);
+    }
+}
